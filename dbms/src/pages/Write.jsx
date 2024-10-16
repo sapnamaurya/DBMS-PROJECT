23dfab7c41583946d6091a18c4201057cbf2c196
sapna maurya <138869250+sapnamaurya@users.noreply.github.com>
2024-10-16 17:51:30 +0530

    write

--- dbms/src/pages/Write.jsx
+++ dbms/src/pages/Write.jsx
@@ -1,7 +1,68 @@
-import React from "react";
-
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import "./write.css";
 const Write = () => {
-  return <div>write</div>;
+  const [value, setValue] = useState("");
+  console.log(value);
+  return (
+    <div className="add">
+      <div className="contnt">
+        <input type="text" placeholder="Title" />
+        <div className="editorContainer">
+          <ReactQuill
+            className="editor"
+            theme="snow"
+            value={value}
+            onChange={setValue}
+          />
+        </div>{" "}
+      </div>
+      <div className="menus">
+        <div className="item">
+          <h1>Publish</h1>
+          <span>
+            <b>Status: </b> Draft
+          </span>
+          <span>
+            <b>Visibility: </b> Public
+          </span>
+          <input style={{ display: "none" }} type="file" id="file" name="" />
+          <label className="file" htmlFor="file">
+            Upload Image
+          </label>
+          <div className="buttons">
+            <button>Save as a draft</button>
+            <button>Publish</button>
+          </div>
+        </div>
+        <div className="item">
+          <h1>Category</h1>
+          <div className="cat">
+            <input type="radio" name="cat" value=" Medical" id=" Medical" />
+            <label htmlFor=" Medical"> Medical tourism</label>
+          </div>
+          <div className="cat">
+            <input type="radio" name="cat" value="Wellness " id="Wellness " />
+            <label htmlFor="Wellness ">Wellness tourism</label>
+          </div>
+          <div className="cat">
+            <input type="radio" name="cat" value="Religious " id="Religious " />
+            <label htmlFor="Religious ">Religious tourism</label>
+          </div>
+          <div className="cat">
+            <input type="radio" name="cat" value="Dark " id="Dark " />
+            <label htmlFor="Dark ">Dark tourism</label>
+          </div>
+
+          <div className="cat">
+            <input type="radio" name="cat" value="food" id="food" />
+            <label htmlFor="food">Food</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Write;
