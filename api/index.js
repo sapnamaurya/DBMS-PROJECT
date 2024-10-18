40bbcf69fada51f17f2895b77e30c606bcd1e31b
sapna maurya <138869250+sapnamaurya@users.noreply.github.com>
2024-10-18 11:32:50 +0530

    login

--- api/index.js
+++ api/index.js
@@ -2,9 +2,10 @@ import express from "express";
 import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
-
+// import cookieParser from "cookie-Parser";
 const app = express();
 app.use(express.json());
+// app.use(cookieParser());
 
 app.use("/api/posts", postRoutes);
 app.use("/api/users", userRoutes);
