02a8e001ff08545ece56bf2d9da51f3f28fb2a6d
sapna maurya <138869250+sapnamaurya@users.noreply.github.com>
2024-10-22 17:57:47 +0530

    context

--- dbms/src/pages/Login.jsx
+++ dbms/src/pages/Login.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 
 import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
 
 const Login = () => {
   const [inputs, setInputs] = useState({
@@ -12,7 +13,7 @@ const Login = () => {
 
   const navigate = useNavigate();
 
-  // const { login } = useContext(AuthContext);
+  const { login } = useContext(AuthContext);
 
   const handleChange = (e) => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
@@ -21,7 +22,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("/auth/login", inputs);
+      await login(inputs);
       navigate("/");
     } catch (err) {
       setError(err.response.data);
